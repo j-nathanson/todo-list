8abfc5a4c65ca34d56bad7328ecea0f2101036a9
jacobnathanson <jacobnath93@gmail.com>
2021-11-08 22:20:00 -0500

    consolidated item circle class. Added green check hover effect

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const submitBtn = document.getElementById("submit");
 const notDoneList = document.getElementById("todoNotDone");
 
 // Event LISTENERS
-submitBtn.addEventListener("click",addTodo) 
+submitBtn.addEventListener("click", addTodo)
 
 // FUNCTIONS
-function addTodo(event){
+function addTodo(event) {
     event.preventDefault();
     const item = document.createElement("div")
-item.innerHTML+= `<li class="list-group-item d-flex justify-content-between">
-<button type="button" class="btn btn-empty btn-circle "></button>
-<p class="my-auto">${input.value}</p>
-<button type="button" class="btn btn-danger btn ">x</button>
-</li>`;
-notDoneList.appendChild(item)
+    item.innerHTML += `<li class="list-group-item d-flex justify-content-between"><button type="button" class="btn btn-circle "></button><p class="my-auto">${input.value}</p><button type="button" class="btn btn-danger btn ">x</button></li>`;
+    notDoneList.appendChild(item)
+    const itemButtons = item.querySelectorAll("button");
+
+    const circleBtn = itemButtons[0];
+    const exitBtn = itemButtons[1];
+
+    exitBtn.addEventListener("click", function () {
+        item.remove();
+    });
     input.value = "";
 }
-
-
