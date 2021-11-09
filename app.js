1019a5b2bfe21a967da1b05bd79805d3d1370290
jacobnathanson <jacobnath93@gmail.com>
2021-11-08 23:01:59 -0500

    can toggle task between sections by clicking the circle

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const input = document.getElementById("input");
 const submitBtn = document.getElementById("submit");
 const notDoneList = document.getElementById("todoNotDone");
+const doneList = document.getElementById("todoDone");
 
 // Event LISTENERS
 submitBtn.addEventListener("click", addTodo)
@@ -12,13 +13,26 @@ function addTodo(event) {
     const item = document.createElement("div")
     item.innerHTML += `<li class="list-group-item d-flex justify-content-between"><button type="button" class="btn btn-circle "><i class="fa fa-check"></i></button><p class="my-auto">${input.value}</p><button type="button" class="btn btn-danger btn ">x</button></li>`;
     notDoneList.appendChild(item)
-    const itemButtons = item.querySelectorAll("button");
 
+    // SELECTORS FOR EACH TASK ITEM
+    const itemButtons = item.querySelectorAll("button");
     const circleBtn = itemButtons[0];
     const exitBtn = itemButtons[1];
 
+    // Delete Task
     exitBtn.addEventListener("click", function () {
         item.remove();
     });
+    // Toggle Sections
+    circleBtn.addEventListener("click", function(){
+       
+        console.log(item.parentNode.id)
+        let sectionID = (item.parentNode.id);
+        if(sectionID === "todoNotDone"){
+             doneList.append(item);
+        }else{
+            notDoneList.append(item);
+        }
+   })
     input.value = "";
 }
