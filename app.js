39fa028659fd03aee93f549001e767aabb66c669
jacobnathanson <jacobnath93@gmail.com>
2021-11-08 22:09:35 -0500

    created basic dom manipulation, can now delete items

---
+++ app.js
@@ -0,0 +1,22 @@
+// SELECTORS
+const input = document.getElementById("input");
+const submitBtn = document.getElementById("submit");
+const notDoneList = document.getElementById("todoNotDone");
+
+// Event LISTENERS
+submitBtn.addEventListener("click",addTodo) 
+
+// FUNCTIONS
+function addTodo(event){
+    event.preventDefault();
+    const item = document.createElement("div")
+item.innerHTML+= `<li class="list-group-item d-flex justify-content-between">
+<button type="button" class="btn btn-empty btn-circle "></button>
+<p class="my-auto">${input.value}</p>
+<button type="button" class="btn btn-danger btn ">x</button>
+</li>`;
+notDoneList.appendChild(item)
+    input.value = "";
+}
+
+
