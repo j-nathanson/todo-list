d3e95561d3fa834fc40cb103fd481efcdb053e43
jacobnathanson <jacobnath93@gmail.com>
2021-11-11 13:47:14 -0500

    addjusted some styling, classes, and make css smaller

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const submitBtn = document.getElementById("submit");
 const notDoneList = document.getElementById("todoNotDone");
 const doneList = document.getElementById("todoDone");
 const numTasksDone = document.getElementById("numFinished");
-let counterDone = 0;
 const numTasksNotDone = document.getElementById("numUnfinished");
+let counterDone = 0;
 let counterNotDone = 0;
 // Event LISTENERS
 submitBtn.addEventListener("click", addTodo)
@@ -14,7 +14,13 @@ submitBtn.addEventListener("click", addTodo)
 function addTodo(event) {
     event.preventDefault();
     const item = document.createElement("div")
-    item.innerHTML += `<li class="list-group-item d-flex justify-content-between"><button type="button" class="btn btn-circle fill-off"><i class="fa fa-check"></i></button><p class="my-auto">${input.value}</p><button type="button" class="btn btn-danger btn ">x</button></li>`;
+    item.innerHTML +=
+        `<li class="list-group-item d-flex justify-content-between">
+            <button type="button" class="btn btn-circle fill-on">
+                <i class="fa fa-check"></i></button>
+                <p class="my-auto">${input.value}</p>
+            <button type="button" class="btn btn-danger btn ">x</button>
+        </li>`;
     notDoneList.appendChild(item)
 
     // SELECTORS FOR EACH TASK ITEM
@@ -27,41 +33,21 @@ function addTodo(event) {
         item.remove();
 
 
-        // counterNotDone--;
-        // counterDone--;
-        // if (counterNotDone < 1) {
-        //     counterNotDone = '';
-        //     numTasksNotDone.innerText = counterNotDone;
-        // } else if (counterDone < 1) {
-        //     counterNotDone = '';
-        //     numTasksDone.innerText = counterDone;
-        // } else{
-        //     numTasksNotDone.innerText = `(${counterNotDone})`;
-        //     numTasksDone.innerText = `(${counterDone})`;
-        // }
-
     });
     // Toggle Sections and Fill color
     circleBtn.addEventListener("click", function () {
-
 
         let sectionID = (item.parentNode.id);
         // switch to Done section
         if (sectionID === "todoNotDone") {
             circleBtn.classList.toggle("bg-success");
-            circleBtn.classList.toggle("fill-off");
+            circleBtn.classList.toggle("fill-on");
             doneList.append(item);
-
-            // counterDone++;
-            // counterNotDone++
-            // numTasksDone.innerHTML = `<h4>(${counterDone})</h4>`;
-            // numTasksNotDone.innerText = `(${counterNotDone})`;
-
 
             //   return to Not Done section
         } else {
             circleBtn.classList.toggle("bg-success");
-            circleBtn.classList.toggle("fill-off");
+            circleBtn.classList.toggle("fill-on");
             notDoneList.append(item);
 
             // counterDone--;
@@ -73,4 +59,8 @@ function addTodo(event) {
         }
     })
     input.value = "";
+}
+
+function decreaseCounter() {
+
 }
